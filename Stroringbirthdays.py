172b9aa843605188d2f606d90367dedc8c309f53
Rakshit Singh <singhrajeshkumar493@gmail.com>
2025-12-01 20:24:35 +0530

    Add files via upload

---
+++ Stroringbirthdays.py
@@ -0,0 +1,8 @@
+date_mom = 2
+date_dad = 28
+month_mom = "July"
+month_dad = "January"
+print("Birthday date of mom is",date_mom)
+print("Birthday month of mom is",month_mom)
+print("Birthday date of dad is",date_dad)
+print("Birthday month of dad is",month_dad)

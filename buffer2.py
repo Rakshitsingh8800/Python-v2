5c161e7e9119fd9ebf91d207c9ef6f0b7ace6c4f
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-01-19 19:39:04 +0530

    Add files via upload

---
+++ buffer2.py
@@ -0,0 +1,11 @@
+def find_first_set_bit(n):
+    if n == 0:
+        return -1
+    position = 0
+    while (n & 1) == 0:
+        n >>= 1
+        position += 1
+        return position
+number = 8
+find_first_set_position = find_first_set_bit(number)
+print(f"First set bit position: {find_first_set_position}")

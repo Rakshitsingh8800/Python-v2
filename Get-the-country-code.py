126358caf891a84668f6097d8868a2059db78497
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-02-09 19:38:49 +0530

    Add files via upload

---
+++ Get-the-country-code.py
@@ -0,0 +1,8 @@
+SPCMcountry_code = {'India' : '0091', 'Austalia' : "0025", 'Nepal' : '00977'}
+
+print("Counrty code for India -")
+print(SPCMcountry_code.get('India', 'Not found'))
+
+
+print("Counrty code for Japan -")
+print(SPCMcountry_code.get('Japan', 'Not found'))

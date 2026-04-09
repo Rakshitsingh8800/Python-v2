2f87d493d05a8c8034e459ce2b2a21d43a04eb6e
Rakshit Singh <singhrajeshkumar493@gmail.com>
2026-04-09 20:39:17 +0530

    Update Age Calculator App.py

--- Age Calculator App.py
+++ Age Calculator App.py
@@ -1,36 +1,25 @@
-from tkinter import *
-
-root = Tk()
-root.title("Age Calculator App")
-root.geometry("400x400")
-
-frame = Frame(master=root, height=200, width=360, bg="lightblue")
-
-lbl1 = Label(frame, text="Enter Your Birth Year", bg="lightblue", fg="white", width=20)
-lbl2 = Label(frame, text="Enter Current Year", bg="lightblue", fg="white", width=20)
-lbl3 = Label(frame, text="Your Age is", bg="lightblue", fg="white", width=20)
+import tkinter as tk
+from datetime import datetime
 
 def calculate_age():
-    birth_year = int(birth_year_entry.get())
-    current_year = int(current_year_entry.get())
-    age = current_year - birth_year
-    age_entry.delete(0, END)
-    age_entry.insert(0, str(age))
-birth_year_entry = Entry(frame)
-current_year_entry = Entry(frame)
-age_entry = Entry(frame)
+    try:
+        dob = datetime.strptime(e.get(), "%d/%m/%Y")
+        t = datetime.today()
+        age = t.year - dob.year - ((t.month, t.day) < (dob.month, dob.day))
+        l.config(text=f"Age: {age} years")
+    except:
+        l.config(text="Enter in DD/MM/YYYY")
 
-btn = Button(text = "Calculate Age", command=calculate_age, bg="red")
+r = tk.Tk()
+r.title("Age Calculator")
+r.geometry("400x400")
 
+tk.Label(r, text="DOB (DD/MM/YYYY)").pack()
+e = tk.Entry(r)
+e.pack()
 
-frame.place(x=20, y=0)
-lbl1.place(x=20, y=20)
-birth_year_entry.place(x=200, y=20)
-lbl2.place(x=20, y=80)
-current_year_entry.place(x=200, y=80)
-lbl3.place(x=20, y=140)
-age_entry.place(x=200, y=140)
-btn.place(x=130, y=210)
+tk.Button(r, text="Calculate", command=calculate_age).pack(pady=5)
+l = tk.Label(r)
+l.pack()
 
-root.mainloop()
-
+r.mainloop()
